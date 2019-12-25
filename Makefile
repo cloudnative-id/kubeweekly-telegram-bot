4d3e59610c24d64c6d788f33a188bbf5dbce32f6
Irvi Firqotul Aini <irvi.fa@gmail.com>
2019-12-25 17:17:07 +0700

    Restructure directory, add Dockerfile, and add .gitignore.

---
+++ Makefile
@@ -0,0 +1,11 @@
+DOCKER_REGISTRY := 
+DOCKER_VERSION := $(shell git rev-parse --short HEAD)
+APPS := kubeweekly-telegram-bot
+
+.PHONY: build
+build: 
+	docker build -t $(DOCKER_REGISTRY)/$(APPS):$(DOCKER_VERSION) .
+
+.PHONY: push
+push:
+	docker push $(DOCKER_REGISTRY)/$(APPS):$(DOCKER_VERSION)
--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
 import telegram
+import os
 from flask import Flask, request
 from jinja2 import Environment, FileSystemLoader
 
-# set current directory
+# Set current directory.
 ENV = Environment(loader=FileSystemLoader('.'))
+# Jinja template of the bot.
+TEMPLATE_PATH = './templates/template_message.j2'
 
-# get template
-baseline = ENV.get_template("./template_message.j2")
+# Load Jinja template.
+baseline = ENV.get_template()
 
 # flask
 app = Flask(__name__) 
 
 # telegram bot
-bot = telegram.Bot('TOKEN')
+telegram_token = os.getenv('TOKEN')
+chat_id = os.getenv('CHATID')
+bot = telegram.Bot(telegram_token)
 
 def telegram_kubeweekly(data):
     config = baseline.render(data=data)
-    bot.send_message(chat_id=-"CHATID", text=config, parse_mode=telegram.ParseMode.MARKDOWN)
+    bot.send_message(chat_id=-chat_id, text=config, parse_mode=telegram.ParseMode.MARKDOWN)
 
 @app.route('/content/kubeweekly', methods=['POST'])
 def kubeweekly():
